478db80aa4f23c4110de858e8221e4ab2793d157
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-14 16:06:50 +0100

    Created container sub-folders and file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import { About, Header, Featured, Footer, Projects, Skills } from "./container";
+
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <About />
+      <Header />
+      <Featured />
+      <Footer />
+      <Projects />
+      <Skills />
     </div>
   );
 };
