63e9cf1f36247c3c9a346a90f55d339cc295db31
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-14 20:30:10 +0100

    Added structure to header.jsx and created css file

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./Header.css";
+
+// images
 import bavAvatar from "../../assets/bav-avatar.jpg";
+import javascriptGlow from "../../assets/javascript-glow.png";
+import reactGlow from "../../assets/react-glow.png";
+import lightBulb from "../../assets/light-bulb.png";
 
 const Header = () => {
   return (
@@ -8,7 +14,13 @@ const Header = () => {
       <div className="header">
         <div className="header__container">
           <a href="#about">
-            <div className="header__badge">
+            <motion.div
+              className="header__badge"
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.3 },
+              }}
+            >
               <div>
                 <img className="memoji" src={bavAvatar} />
               </div>
@@ -16,8 +28,25 @@ const Header = () => {
                 <p className="p-text">Hello, I'm</p>
                 <h1 className="head-text">Bav Kudhail</h1>
               </div>
-            </div>
+            </motion.div>
           </a>
+        </div>
+        {/* second half of code */}
+        <div className="blob-container">
+          <div className="tech-logo bulb-logo">
+            <img src={lightBulb} />
+          </div>
+          <div className="tech-logo react-logo">
+            <img className="tech-logo-img" src={reactGlow} />
+          </div>
+          <div className="tech-logo javascript-logo">
+            <img className="tech-logo-img" src={javascriptGlow} />
+          </div>
+          <motion.div
+            className="fixed-blur"
+            whileInView={{ scale: [0, 1] }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+          />
         </div>
       </div>
     </>
