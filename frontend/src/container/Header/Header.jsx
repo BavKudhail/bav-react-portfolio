0f88c3a63784d6ba77d7a3c80781116afdd81109
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-14 20:03:32 +0100

    created header badge and header.css styling

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Header.css";
+import bavAvatar from "../../assets/bav-avatar.jpg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="header">
+        <div className="header__container">
+          <a href="#about">
+            <div className="header__badge">
+              <div>
+                <img className="memoji" src={bavAvatar} />
+              </div>
+              <div>
+                <p className="p-text">Hello, I'm</p>
+                <h1 className="head-text">Bav Kudhail</h1>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
