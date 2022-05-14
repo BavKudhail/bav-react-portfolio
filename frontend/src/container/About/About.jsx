8166662607ed0a707ee1879cd110214e403ddfa2
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-14 21:37:48 +0100

    About me page completed

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.css";
+import { Wrapper } from "../../hoc";
 import aboutAvatar from "../../assets/about_avatar.JPG";
 
 // images
@@ -14,23 +15,26 @@ const About = () => {
           </h2>
         </div>
         <div className="about__container">
-          <img src={aboutAvatar} />
-        </div>
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <h5 className="sub-text">About Me</h5>
-              <p className="about-text p-text">
-                I am an Architectural designer turned Software Developer.
-                Currently enrolled in a full-stack web development course at The
-                University of Birmingham. I am very curious by nature, learning
-                new technologies and solving problems is what makes me excited
-                to get out of bed in the morning. My curiosity combined with my
-                passion for building drives me to ponder the future of tech, and
-                bring together design and code to build elegant web
-                applications.
-              </p>
-            </article>
+          <div className="about__me">
+            <img src={aboutAvatar} />
+            <div className="about__me-background" />
+          </div>
+          <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <h5 className="sub-text">About Me</h5>
+                <p className="p-text">
+                  I am an Architectural designer turned Software Developer.
+                  Currently enrolled in a full-stack web development course at
+                  The University of Birmingham. I am very curious by nature,
+                  learning new technologies and solving problems is what makes
+                  me excited to get out of bed in the morning. My curiosity
+                  combined with my passion for building drives me to ponder the
+                  future of tech, and bring together design and code to build
+                  elegant web applications.
+                </p>
+              </article>
+            </div>
           </div>
         </div>
         <div className="btn-container">
@@ -41,4 +45,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Wrapper(About, "about");
