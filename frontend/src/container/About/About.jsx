e4e8c96ceeb04ecdf0ff32ef301daba8a47f8c2f
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-14 20:54:43 +0100

    built about.jsx structure

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 import "./About.css";
+import aboutAvatar from "../../assets/about_avatar.JPG";
+
+// images
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <>
+      <div className="about">
+        <div>
+          <h2 className="head-text">
+            I am a creative developer. <br></br> With a designers eye.{" "}
+          </h2>
+        </div>
+        <div className="about__container">
+          <img src={aboutAvatar} />
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <h5 className="sub-text">About Me</h5>
+              <p className="about-text p-text">
+                I am an Architectural designer turned Software Developer.
+                Currently enrolled in a full-stack web development course at The
+                University of Birmingham. I am very curious by nature, learning
+                new technologies and solving problems is what makes me excited
+                to get out of bed in the morning. My curiosity combined with my
+                passion for building drives me to ponder the future of tech, and
+                bring together design and code to build elegant web
+                applications.
+              </p>
+            </article>
+          </div>
+        </div>
+        <div className="btn-container">
+          <button>Resume</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
