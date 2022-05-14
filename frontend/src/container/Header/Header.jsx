81815b93d4eb84afc0e4fa6394940474c6b87b2d
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-14 20:36:12 +0100

    Created Wrapper HOC and corresponding styling

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./Header.css";
+import { Wrapper } from "../../hoc";
 
 // images
 import bavAvatar from "../../assets/bav-avatar.jpg";
@@ -13,25 +14,26 @@ const Header = () => {
     <>
       <div className="header">
         <div className="header__container">
-          <a href="#about">
-            <motion.div
-              className="header__badge"
-              whileHover={{
-                scale: 1.1,
-                transition: { duration: 0.3 },
-              }}
-            >
-              <div>
-                <img className="memoji" src={bavAvatar} />
-              </div>
-              <div>
-                <p className="p-text">Hello, I'm</p>
-                <h1 className="head-text">Bav Kudhail</h1>
-              </div>
-            </motion.div>
-          </a>
+          <div>
+            <a href="#about">
+              <motion.div
+                className="header__badge"
+                whileHover={{
+                  scale: 1.1,
+                  transition: { duration: 0.3 },
+                }}
+              >
+                <div>
+                  <img className="memoji" src={bavAvatar} />
+                </div>
+                <div>
+                  <p className="p-text">Hello, I'm</p>
+                  <h1 className="head-text">Bav Kudhail</h1>
+                </div>
+              </motion.div>
+            </a>
+          </div>
         </div>
-        {/* second half of code */}
         <div className="blob-container">
           <div className="tech-logo bulb-logo">
             <img src={lightBulb} />
@@ -53,4 +55,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default Wrapper(Header, "home");
