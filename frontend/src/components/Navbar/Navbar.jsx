40a1e55fe9c2cbbaad903572244547b164d687ba
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-14 19:15:25 +0100

    added navbar.css styling and added navbar logo + classNames

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,15 +5,22 @@ const Navbar = () => {
   const navLinks = ["home", "about", "work", "skills", "contact"];
 
   return (
-    <ul>
-      {navLinks.map((link) => {
-        return (
-          <li key={link}>
-            <a href={`#${link}`}>{link}</a>
-          </li>
-        );
-      })}
-    </ul>
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <div className="navbar__logo-container">
+          <span>BK</span>
+        </div>
+      </div>
+      <ul className="navbar__links">
+        {navLinks.map((link) => {
+          return (
+            <li key={link} className="navbar_link">
+              <a href={`#${link}`}>{link}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
