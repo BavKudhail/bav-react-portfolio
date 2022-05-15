7f4fe4c41de85b79fabe64cdad7d661348ebb123
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-15 15:43:09 +0100

    created syntax for contact form

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -21,6 +21,33 @@ const Footer = () => {
             <a href="tel: +44 (774653912)">07446835912</a>
           </div>
         </div>
+        {/* contact form */}
+        <div className="contact__form app__flex">
+          <div className="app__flex">
+            <input
+              className="p-text input-field"
+              name="name"
+              type="text"
+              placeholder="Your Name"
+            />
+          </div>
+          <input
+            className="p-text input-field"
+            name="name"
+            type="text"
+            placeholder="Your Name"
+          />
+        </div>
+        <div className="app__flex">
+          <textarea
+            className="p-text input-field "
+            placeholder="Your Message"
+            name="message"
+          />
+        </div>
+        <button type="button" className="msg-button">
+          Send Message
+        </button>
       </div>
     </>
   );
