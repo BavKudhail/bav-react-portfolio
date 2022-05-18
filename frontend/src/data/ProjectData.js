3564307c35efda054030d1141f1dcaa58bb8515b
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-18 18:22:37 +0100

    created ProjectData.js file

---
+++ frontend/src/data/ProjectData.js
@@ -0,0 +1,36 @@
+import macroMeals from "../assets/macromeals.jpg";
+
+const ProjectData = [
+  {
+    id: "1",
+    title: "Creator Labs",
+    description:
+      "A platform for creatives to network, share skills and build new and exciting projects.",
+    image: macroMeals,
+    githubUrl: "https://github.com/BavKudhail/creative-labs",
+    deployedUrl: "https://creativelabs-app.herokuapp.com/",
+    tags: ["Full-stack", "All"],
+  },
+  {
+    id: "2",
+    title: "Creator Labs",
+    description:
+      "A platform for creatives to network, share skills and build new and exciting projects.",
+    image: macroMeals,
+    githubUrl: "https://github.com/BavKudhail/creative-labs",
+    deployedUrl: "https://creativelabs-app.herokuapp.com/",
+    tags: ["Full-stack", "All"],
+  },
+  {
+    id: "3",
+    title: "Creator Labs",
+    description:
+      "A platform for creatives to network, share skills and build new and exciting projects.",
+    image: macroMeals,
+    githubUrl: "https://github.com/BavKudhail/creative-labs",
+    deployedUrl: "https://creativelabs-app.herokuapp.com/",
+    tags: ["Full-stack", "All"],
+  },
+];
+
+export default ProjectData;
