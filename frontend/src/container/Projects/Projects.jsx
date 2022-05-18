c7d8a91e240666cc30fb667fe5ed9fa798efb122
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-18 18:29:13 +0100

    project data is now dynamically displayed on front-end

--- frontend/src/container/Projects/Projects.jsx
+++ frontend/src/container/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import { Wrapper } from "../../hoc";
 import { motion } from "framer-motion";
 import defaultIMG from "../../assets/defaultIMG.jpg";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import ProjectData from "../../data/ProjectData";
 
 const Projects = () => {
   const filterButtons = [
@@ -24,21 +25,29 @@ const Projects = () => {
           <h4 className="sub-text">Others worth a mention</h4>
         </div>
         <div className="projects__filter">
-          {filterButtons.map((button) => {
-            return <div className="projects__filter-button">{button}</div>;
+          {filterButtons.map((button, index) => {
+            return (
+              <div className="projects__filter-button" key={index}>
+                {button}
+              </div>
+            );
           })}
         </div>
         {/* project card */}
         <motion.div className="projects__card-container">
-          <div className="projects-item app__flex">
-            <div className="projects-img app__flex">
-              <img src={defaultIMG} />
-            </div>
-            <div className="projects-content app__flex">
-              <h4 className="bold-text">Project Title</h4>
-              <p className="p-text">Project Description</p>
-            </div>
-          </div>
+          {ProjectData.map((project) => {
+            return (
+              <div className="projects-item app__flex">
+                <div className="projects-img app__flex">
+                  <img src={project.image} />
+                </div>
+                <div className="projects-content app__flex">
+                  <h4 className="bold-text">{project.title}</h4>
+                  <p className="p-text">{project.description}</p>
+                </div>
+              </div>
+            );
+          })}
         </motion.div>
       </div>
     </>
