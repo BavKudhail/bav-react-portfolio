5ca1cf5f3541436128cb56bfd9df8b3bca9016b0
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-18 17:42:38 +0100

    updated featuredData.js content

--- frontend/src/data/FeaturedData.js.js
+++ frontend/src/data/FeaturedData.js.js
@@ -1,4 +1,6 @@
 import Profile from "../assets/Profile.gif";
+import macroMeals from "../assets/macromeals.jpg";
+import techtab from "../assets/techtab.JPG";
 
 // name
 // description
@@ -11,20 +13,39 @@ const FeaturedData = [
   {
     id: "1",
     title: "Creator Labs",
-    description: "Project description",
+    description:
+      "A platform for creatives to network, share skills and build new and exciting projects.",
     image: Profile,
-    githubUrl: "www.github.com/bavkudhail",
-    deployedUrl: "www.github.com/bavkudhail",
-    techStack: ["React", "Javascript", "CSS"],
+    githubUrl: "https://github.com/BavKudhail/creative-labs",
+    deployedUrl: "https://creativelabs-app.herokuapp.com/",
+    techStack: [
+      "Javascript",
+      "jQuery",
+      "Express",
+      "MySQL",
+      "AWS S3",
+      "Socket.io",
+    ],
   },
   {
     id: "2",
     title: "Macro Meals",
-    description: "Project description",
-    image: Profile,
-    githubUrl: "www.github.com/bavkudhail",
-    deployedUrl: "www.github.com/bavkudhail",
-    techStack: ["React", "Javascript", "CSS"],
+    description:
+      "A meal-planner app for the health and fitness conscious consumer with a busy schedule.",
+    image: macroMeals,
+    githubUrl: "https://github.com/BavKudhail/macro-meals",
+    deployedUrl: "https://bavkudhail.github.io/macro-meals/",
+    techStack: ["Javascript", "jQuery", "API", "TailwindCSS"],
+  },
+  {
+    id: "3",
+    title: "Tech Blog",
+    description:
+      "CMS style blog for developers to publish blog posts and comment on other posts.",
+    image: techtab,
+    githubUrl: "https://github.com/BavKudhail/tech_blog_mvc",
+    deployedUrl: "https://techtab-blog.herokuapp.com/",
+    techStack: ["Javascript", "Express", "NodeJS", "MySQL", "TailwindCSS"],
   },
 ];
 
