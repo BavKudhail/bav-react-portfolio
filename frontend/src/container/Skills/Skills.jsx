5167081c0d41d24861444d3efb1d55c5f629df2f
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-18 20:06:55 +0100

    created dynamic skills section

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -3,6 +3,7 @@ import "./Skills.css";
 import { motion } from "framer-motion";
 import reactIcon from "../../assets/react.png";
 import { Wrapper } from "../../hoc";
+import SkillsData from "../../data/SkillsData";
 
 const Skills = () => {
   return (
@@ -11,14 +12,17 @@ const Skills = () => {
         <h2 className="head-text">Skills</h2>
         <div className="skills__container">
           <div className="skills-list">
-            {/* map over each return motion div */}
-            <motion.div className="skills-item app__flex">
-              <div className="app__flex">
-                <img className="skill-img" src={reactIcon} />
-                <img className="skill-img blur" src={reactIcon} />
-              </div>
-              <p>React</p>
-            </motion.div>
+            {SkillsData.map((skill) => {
+              return (
+                <motion.div className="skills-item app__flex">
+                  <div className="app__flex">
+                    <img className="skill-img" src={skill.icon} />
+                    <img className="skill-img blur" src={skill.icon} />
+                  </div>
+                  <p className="p-text">{skill.name}</p>
+                </motion.div>
+              );
+            })}
           </div>
         </div>
       </div>
---
+++ frontend/src/data/SkillsData.js
@@ -0,0 +1,41 @@
+// images
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import react from "../assets/react.png";
+import graphql from "../assets/graphql.png";
+import node from "../assets/node.png";
+import redux from "../assets/redux.png";
+import sass from "../assets/sass.png";
+
+const SkillsData = [
+  {
+    name: "React",
+    icon: react,
+  },
+  {
+    name: "javascript",
+    icon: javascript,
+  },
+  {
+    name: "Redux",
+    icon: redux,
+  },
+  {
+    name: "node",
+    icon: node,
+  },
+  {
+    name: "sass",
+    icon: sass,
+  },
+  {
+    name: "graphql",
+    icon: graphql,
+  },
+  {
+    name: "html",
+    icon: html,
+  },
+];
+
+export default SkillsData;
